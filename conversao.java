172cc90de2cf05be26b4a4a4c445bf2c6767338d
BEATRIZ PIFFER SILVA <81167411+Beatrizpiffer@users.noreply.github.com>
2022-04-18 09:30:02 -0300

    Create conversao.java

---
+++ conversao.java
@@ -0,0 +1,22 @@
+public class TestaConversao {
+  
+   public static void main(String[] args) {
+       double salario = 1270.50
+
+       float pontoFlutuante = 3.14f;
+
+       int valor = (int) salario;
+       System.out.println(valor);
+      
+       long numeroGrande = 322234535445L; numeros grandes 
+       short valorPequeno = 2131; usado para numeros pequenos
+       byte b = 127;
+      
+       double valor1 = 0.2;
+       double valor2 = 0.1;
+       double total = valor1 + valor2;
+
+       System.out.println(total);0.300000000000004
+ 
+    }
+} 
